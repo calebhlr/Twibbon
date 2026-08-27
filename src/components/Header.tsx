04dfbe44c4d515b307024ada4bf0198bf69dd953
Caleb <88343553+calebhlr@users.noreply.github.com>
2026-08-27 15:20:11 -0300

    Update Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,3 @@
 export function Header() {
-  return <header className="header"><p className="eyebrow">#GANDINI20000</p><h1>DEPUTADO<br />GANDINI <em>20.000</em></h1><p className="subtitle">Faça sua foto, escolha sua moldura e compartilhe essa ideia.</p></header>
+  return <header className="header"><p className="eyebrow">#CAMPLY90000</p><h1>DEPUTADO<br />GANDINI <em>20.000</em></h1><p className="subtitle">Faça sua foto, escolha sua moldura e compartilhe essa ideia.</p></header>
 }
