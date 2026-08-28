afbc3c65a4d5d691e1ac4f2e93b8ccc24e2abda3
Caleb <88343553+calebhlr@users.noreply.github.com>
2026-08-28 01:04:36 -0300

    Add Camply campaign frame

---
+++ src/components/Footer.tsx
@@ -0,0 +1,3 @@
+export function Footer() {
+  return <footer><strong>Seu dado é seu.</strong> A sua imagem é processada somente no seu dispositivo e não é enviada para nenhum servidor. <a href="#privacidade">Privacidade LGPD</a></footer>
+}
